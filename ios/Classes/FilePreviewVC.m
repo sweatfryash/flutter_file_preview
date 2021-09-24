16394c27507c7eb08de7a1027ed238d91bb28621
hch <342254689@qq.com>
2021-09-24 19:28:52 +0800

    Revert "✨ 替换UIWebView为WKWebView"
    This reverts commit a9d17b3b5b711b6df1f0ebe700e9901164a86f5a.

--- ios/Classes/FilePreviewVC.m
+++ ios/Classes/FilePreviewVC.m
@@ -6,10 +6,9 @@
 //
 
 #import "FilePreviewVC.h"
-#import <WebKit/WebKit.h>
 
 @interface FilePreviewVC ()
-@property (nonatomic, strong) WKWebView *myWebView;
+@property (nonatomic, strong) UIWebView *myWebView;
 @end
 
 @implementation FilePreviewVC
@@ -23,8 +22,8 @@
   [backBtn addTarget:self action:@selector(close) forControlEvents:UIControlEventTouchUpInside];
   self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backBtn];
   self.navigationController.navigationBar.backgroundColor = [UIColor whiteColor];
-  self.myWebView = [[WKWebView alloc] initWithFrame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-//  self.myWebView.scalesPageToFit = YES;//使文档的显示范围适合UIWebView的bounds
+  self.myWebView = [[UIWebView alloc] initWithFrame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+  self.myWebView.scalesPageToFit = YES;//使文档的显示范围适合UIWebView的bounds
   [self.view addSubview:self.myWebView];
 }
 
@@ -60,3 +59,4 @@
 }
 
 @end
+
