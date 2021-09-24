776419eb38c84863f3aadb7b31e31a4ae7cc33de
hch <342254689@qq.com>
2021-09-24 23:55:12 +0800

    删除 ios

--- ios/Classes/FlutterFilePreviewPlugin.h
+++
@@ -1,6 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterFilePreviewPlugin : NSObject<FlutterPlugin>
-@property (nonatomic, assign) UIViewController *hostViewController;
-@property (nonatomic, copy) NSString *backImgPath;
-@end
